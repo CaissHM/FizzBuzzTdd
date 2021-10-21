c0217e177dc8eeb16f150249a92c52b77b58da4a
Beatriz de Castro Resende <83736310+beatrizresende@users.noreply.github.com>
2021-10-20 22:32:01 -0300

    Testes unitários e injeção de dependência
    Fazendo testes das condições do FizzBuzz e iniciando injeção de dependência

--- FizzBuzzTdd.Domain/FizzBuzz.cs
+++ FizzBuzzTdd.Domain/FizzBuzz.cs
@@ -4,5 +4,12 @@ namespace FizzBuzzTdd.Domain
 {
     public class FizzBuzz
     {
+        public string Of(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+            else if (num % 3 == 0) return "Fizz";
+            else if (num % 5 == 0) return "Buzz";
+            else return ""; //1º teste: se eu não colocar um retorno vazio em uma linha do código o método não compila - o código não aceita só "return" já que o retorno é "string"
+        }
     }
 }
--- FizzBuzzTdd.UnitTests/FizzBuzzUnitTests.cs
+++ FizzBuzzTdd.UnitTests/FizzBuzzUnitTests.cs
@@ -6,10 +6,107 @@ namespace FizzBuzzTdd.UnitTests
 {
     public class FizzBuzzUnitTests
     {
-        [Fact]
-        public void Test()
+        private FizzBuzz fizzBuzz;
+
+        public FizzBuzzUnitTests()
         {
-            FizzBuzz sut = new FizzBuzz();
+            fizzBuzz = new FizzBuzz(); //coloquei a instância em uma variável que se inicializa no construtor pq o construtor roda antes de cada teste, ou seja, que vou ter uma variável só sendo usada e inicializada em todos os testes ao invés de uma nova instância manual (var sut = new FizzBuzz()) em cada teste
+        }
+
+        [Fact]
+        public void NumberIsMultiple_Of3And5_ReturnsFizzBuzz() //NumberIsMultiple_Of<números que têm que ser múltiplo>_Returns<retorno esperado>
+        {
+            //Assert.Equal("FizzBuzz", fizzBuzz.Of(15));
+
+            //refatorando:
+            //Arrange
+            var expected = "FizzBuzz";
+
+            //Act
+            var actual = fizzBuzz.Of(-15);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NumberIsMultiple_Of3_ReturnsFizz() 
+        {
+            //Assert.Equal("Fizz", fizzBuzz.Of(9));
+
+            //refatorando:
+            //Arrange
+            var expected = "Fizz";
+
+            //Act
+            var actual = fizzBuzz.Of(9);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NumberIsMultiple_Of5_ReturnsBuzz()
+        {
+            //Assert.Equal("Buzz", fizzBuzz.Of(25));
+
+            //refatorando:
+            //Arrange
+            var expected = "Buzz";
+
+            //Act
+            var actual = fizzBuzz.Of(25);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        //[Fact]
+        //public void NumberIsntMultiple_Of3Or5Or3And5_ReturnsNumber()              *método do "Caso contrário, retorna o próprio número."*
+        //{
+        //    //Assert.Equal("Buzz", fizzBuzz.Of(25));
+
+        //    //refatorando:
+        //    //Arrange
+        //    var expected = "Buzz";
+
+        //    //Act
+        //    var actual = fizzBuzz.Of(25);
+
+        //    //Assert
+        //    Assert.Equal(expected, actual);
+        //}
+
+        [Fact]
+        public void NumberIs0_ReturnsFizzBuzz()
+        {
+            Assert.Equal("FizzBuzz", fizzBuzz.Of(0));
+
+            ////refatorando:
+            ////Arrange
+            //var expected = "Buzz";
+
+            ////Act
+            //var actual = fizzBuzz.Of(25);
+
+            ////Assert
+            //Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void NumberIs1_Returns1() //tá dando erro pq depende do "NumberIsntMultiple_Of3Or5Or3And5_ReturnsNumber()"
+        {
+            //Assert.Equal("1", fizzBuzz.Of(1));
+
+            //refatorando:
+            //Arrange
+            var expected = "1";
+
+            //Act
+            var actual = fizzBuzz.Of(1);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }

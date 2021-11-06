030a81956cda2b62c95b00338fea6e920e24e418
Beatriz de Castro Resende <83736310+beatrizresende@users.noreply.github.com>
2021-11-05 21:26:41 -0300

    Adicionando App e injeção de dependência

---
+++ FizzBuzzTdd.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using FizzBuzzTdd.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FizzBuzzTdd.App
+{
+    public class Program
+    {
+        private static readonly CancellationTokenSource tokenSource = new();
+        static Task Main(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder();
+
+            hostBuilder.ConfigureServices((_, services) =>
+            {
+                services.AddTransient<FizzBuzz>();
+                services.AddHostedService<Startup>();
+                services.AddSingleton(tokenSource);
+            });
+
+            using var host = hostBuilder.Build();
+
+            return host.RunAsync(tokenSource.Token);
+        }
+    }
+}
